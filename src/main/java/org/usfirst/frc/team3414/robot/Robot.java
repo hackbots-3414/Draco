5e133756923936fa4dbae89f10237add1766faeb
Preston B <peepsbuzzcut@gmail.com>
2019-01-26 11:12:33 -0500

    testing branching

--- src/main/java/org/usfirst/frc/team3414/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team3414/robot/Robot.java
@@ -24,6 +24,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends IterativeRobot {
+	//testing
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
 	private String m_autoSelected;
