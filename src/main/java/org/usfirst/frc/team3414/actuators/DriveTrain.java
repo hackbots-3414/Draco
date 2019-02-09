56221d92f2f2f8e0cde607fa269bc5b67055d078
DesertThorn1 <fpsfrancism38@stu.fpsk12.net>
2019-02-09 14:42:45 -0500

    Added IR logic from DriveTrain.java

--- src/main/java/org/usfirst/frc/team3414/actuators/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team3414/actuators/DriveTrain.java
@@ -2,11 +2,45 @@ package org.usfirst.frc.team3414.actuators;
 
 import org.usfirst.frc.team3414.config.Config;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+
 public class DriveTrain {
+	
 	double leftJoySpeed = 0;
 	double rightJoySpeed = 0;
+	private static DriveTrain instance;
 	public MultiMotor left = new MultiMotor(Config.LEFT_FRONT,Config.LEFT_MIDDLE,Config.LEFT_REAR);
 	public MultiMotor right = new MultiMotor(Config.RIGHT_FRONT,Config.RIGHT_MIDDLE,Config.RIGHT_REAR);
+	AnalogInput longRangeIRLeft = new AnalogInput(0);
+	AnalogInput longRangeIRRight = new AnalogInput(1);
+	AnalogInput lineSensor = new AnalogInput(2);
+
+	public void autocorrect(){
+	left.set(0);
+    right.set(0);
+  //Timer.delay(1);
+  //Back up robot to white line if it was passed over
+  while (lineSensor.getAverageVoltage() < 1 ) {
+    left.set(-0.10);
+    right.set(-0.10);
+  }
+  //Stop Movement
+  left.set(0);
+  right.set(0);
+  //Rotate robot to left until straight towards target
+  while ((longRangeIRLeft.getAverageVoltage() - longRangeIRRight.getAverageVoltage()) > 0.05) {
+    right.set(-0.25);
+    left.set(0.25);
+  }
+  //Rotate robot to right until straight towards target
+  while ((longRangeIRRight.getAverageVoltage() - longRangeIRLeft.getAverageVoltage()) > 0.05) {
+    right.set(0.25);
+    left.set(-0.25);
+  }
+  // Timer.delay(1);
+}
+
+	
 	
 
 	public void teleop(double leftSpeed, double rightSpeed) {
@@ -36,8 +70,6 @@ public class DriveTrain {
 		right.setInverted(true);
 
 	}
-	private static DriveTrain instance;
-
 		public static DriveTrain getInstance()
 		{
 			if(instance == null)
